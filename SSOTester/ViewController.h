92503137448aa0d6ae915aebfcc175a8c9923a73
Andrew Roper <andrewroper@gmail.com>
2016-04-12 12:44:32 +0200

    Initial Commit

---
+++ SSOTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSOTester
+//
+//  Created by Andres El Ropero on 4/12/16.
+//  Copyright © 2016 Proto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SSOTester/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SSOTester
+//
+//  Created by Andres El Ropero on 4/12/16.
+//  Copyright © 2016 Proto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
